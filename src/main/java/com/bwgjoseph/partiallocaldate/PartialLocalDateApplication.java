c1183dd031eafe294c9cc84f5e65ead4bd204774
bwgjoseph <josephgan@live.com.sg>
2022-08-16 17:48:56 +0800

    convert to a spring-boot project

---
+++ src/main/java/com/bwgjoseph/partiallocaldate/PartialLocalDateApplication.java
@@ -0,0 +1,11 @@
+package com.bwgjoseph.partiallocaldate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartialLocalDateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PartialLocalDateApplication.class, args);
+    }
+}
